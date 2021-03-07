06ab7dbd5b1afec8f2bbea2fe063da142aa161a1
aitormontenegro <aitormontenegro@gmail.com>
2021-03-07 11:01:48 +0100

    Adding support for avif

--- test.go
+++ test.go
@@ -5,19 +5,19 @@ import (
 	"os"
   )
 
+func main(){
 
-buffer, err := bimg.Read("testdata/test.jpg")
-if err != nil {
-  fmt.Fprintln(os.Stderr, err)
+	buffer, err    : = bimg.Read("testdata/test.jpg")
+	if err != nil {
+	  fmt.Fprintln(os.Stderr, err)
+	}
+	newImage, err  : = bimg.NewImage(buffer).Convert(bimg.AVIF)
+	if err != nil {
+	  fmt.Fprintln(os.Stderr, err)
+	}
+	if bimg.NewImage(newImage).Type() == "avif" {
+	  fmt.Fprintln(os.Stderr, "The image was converted into avif")
+	}
+	bimg.Write("/tmp/new.jpg", newImage)
+	
 }
-
-newImage, err := bimg.NewImage(buffer).Convert(bimg.AVIF)
-if err != nil {
-  fmt.Fprintln(os.Stderr, err)
-}
-
-if bimg.NewImage(newImage).Type() == "avif" {
-  fmt.Fprintln(os.Stderr, "The image was converted into avif")
-}
-
-bimg.Write("/tmp/new.jpg", newImage)
