a5d4a13a7fcb6bcee95ace20937347c5e27fe6b2
aitormontenegro <aitormontenegro@gmail.com>
2021-03-07 11:31:41 +0100

    Adding support for avif

--- vips.h
+++ vips.h
@@ -371,6 +371,7 @@ vips_tiffsave_bridge(VipsImage *in, void **buf, size_t *len) {
 int
 vips_avifsave_bridge(VipsImage *in, void **buf, size_t *len, int strip, int quality, int lossless, int speed) {
 #if (VIPS_MAJOR_VERSION > 8 || (VIPS_MAJOR_VERSION >= 8 && VIPS_MINOR_VERSION > 10) || (VIPS_MAJOR_VERSION >= 8 && VIPS_MINOR_VERSION >= 10 && VIPS_MICRO_VERSION >= 2))
+    println("AQUI1");
     return vips_heifsave_buffer(in, buf, len,
     "strip", INT_TO_GBOOLEAN(strip),
     "Q", quality,
@@ -380,6 +381,7 @@ vips_avifsave_bridge(VipsImage *in, void **buf, size_t *len, int strip, int qual
     NULL
     );
 #elif (VIPS_MAJOR_VERSION > 8 || (VIPS_MAJOR_VERSION == 8 && VIPS_MINOR_VERSION >= 9))
+    println("AQUI2");
     return vips_heifsave_buffer(in, buf, len,
     "strip", INT_TO_GBOOLEAN(strip),
     "Q", quality,
